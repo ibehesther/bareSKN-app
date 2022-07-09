66eb84a9b01b27abe9cd926bbffe393d38f10e36
Ibeh Esther <estheribeh5@gmail.com>
2022-07-09 21:57:10 +0100

    built frontend components

---
+++ src/Components/SideBarProductOptions.jsx
@@ -0,0 +1,21 @@
+
+
+function SideBarProductsOptions(props) {
+    const productOptions = [
+        {option: "BY BRAND", suboption: []},
+        {option: "BY OPTION", suboption: []},
+        {option: "BY CONCERN", suboption: []},
+        {option: "BY SKIN TYPE", suboption: []}
+    ]
+    return(
+        <ul className="sidebar-products-option" >
+            {productOptions.map((option, index) => 
+                <li className="sub-products" key={index}>
+                    <span className="sidebar-products-title">{option.option}</span>
+                </li>
+            )}
+        </ul>
+    );
+}
+
+export default SideBarProductsOptions;
