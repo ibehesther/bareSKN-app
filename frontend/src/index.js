4ecf25f75d8fa6e81aeffc99a5a9288d7561a360
Ibeh Esther <estheribeh5@gmail.com>
2022-07-13 18:28:18 +0100

    app routing modification

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter 
-    // basename={'/frontend'}
+    basename={'/bareSKN'}
     >
       <App/>
     </BrowserRouter>
