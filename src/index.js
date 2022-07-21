3225740ffffbd57374de86e7fcad153c2eb4d693
Ibeh Esther <estheribeh5@gmail.com>
2022-07-21 17:16:03 +0100

    fix: Application route

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter 
-    // basename={'/bareSKN'}
+    basename={'/bareSKN'}
     >
       <App/>
     </BrowserRouter>
